aaf4913db1c95de6df9859d2feec40862d71d49b
Suanchik <tamu.suanchik@bk.ru>
2021-09-24 23:17:05 +0300

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./public/db.json');
+const router = jsonServer.router('./src/assets/data.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
